37fcdf5fb6a83eb6dec65935031ab469a90a8274
Dpr1815 <91048238+Dpr1815@users.noreply.github.com>
2022-03-21 17:49:18 +0100

    Update mint1155_factory.sol

--- mint1155_factory.sol
+++ mint1155_factory.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 //import 1155 token contract from Openzeppelin
 
-import "./test.sol";
+import "./mint1155.sol";
  import "github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
 
 
