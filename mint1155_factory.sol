2324c95e78672b8f50b6acadbd937820f72eb266
Dpr1815 <91048238+Dpr1815@users.noreply.github.com>
2022-04-06 19:48:45 +0200

    Update mint1155_factory.sol

--- mint1155_factory.sol
+++ mint1155_factory.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 //import 1155 token contract from Openzeppelin
 
-import "./mint1155.sol";
+import "./test.sol";
  import "github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
 
 
@@ -33,8 +33,8 @@ event MinterRoleAssigned(address minterAddress);
 
      
 
-event ContractCreated(address newAddress, string metadata,string name, string image, address owner, bool refunded, uint256 maxInfected, uint256 itemID);
- function deployCollection(string memory uri,uint256[] memory ids, uint256[] memory amount, string memory name, string memory image,uint256 maxInfected )
+event ContractCreated(address newAddress, string metadata,string name, string image, address owner, bool refunded, uint256 maxInfected,string date, uint256 itemID);
+ function deployCollection(string memory uri,uint256[] memory ids, uint256[] memory amount, string memory name, string memory image,uint256 maxInfected, string memory date )
         public
        
       
@@ -45,7 +45,7 @@ event ContractCreated(address newAddress, string metadata,string name, string im
             uint256 itemId = _itemIds.current();
       address owner = msg.sender; 
      NFTContract Collection = new NFTContract ( uri, ids,amount,owner);  
-    emit ContractCreated(address(Collection),uri,name, image, owner, false,maxInfected, itemId);
+    emit ContractCreated(address(Collection),uri,name, image, owner, false,maxInfected,date, itemId);
 
       
     }
