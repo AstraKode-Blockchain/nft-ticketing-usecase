bfb03d228c00852436f66101eb3d66f086a737d4
Ori Bachar <oribachar98@gmail.com>
2022-11-04 16:22:03 +0200

    Create RefundedData.sol

---
+++ utils/RefundedData.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+library RefundedData {
+
+    struct RefundParameters {
+        uint itemId;
+        address nftContract;
+        address owner;
+        uint256 price;
+        uint256 maxInfection;
+        bool refunded;
+    }
+
+    struct MarketItemUtils {
+        mapping(uint256 => RefundParameters) idRefundParameters;
+    }
+}
