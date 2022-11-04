3dfa555f67ba1ef50a6cc01d2c679136dc4f711d
Ori Bachar <oribachar98@gmail.com>
2022-11-04 11:02:28 +0200

    Update mint1155.sol

--- mint1155.sol
+++ mint1155.sol
@@ -1,27 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "./utilities/ERC1155.sol";
-
 import "./utilities/Ownable.sol";
 
 contract NFTContract is ERC1155, Ownable {
-
-
     constructor(
-         
-         string memory uri,
-         uint256[] memory ids,
-         uint256[] memory amount,
-         address owner
+        string memory uri,
+        uint256[] memory ids,
+        uint256[] memory amount,
+        address owner
     )
-        ERC1155( uri
-            // "https://ipfs.moralis.io:2053/ipfs/QmS7izjgMprD3ZvP8aDBRiXbMZnxBsrUK4PJTwkcDFauij" 
-            )
+        ERC1155(
+            uri
+            // "https://ipfs.moralis.io:2053/ipfs/QmS7izjgMprD3ZvP8aDBRiXbMZnxBsrUK4PJTwkcDFauij"
+        )
     {
-      
-  _mintBatch(owner, ids,amount,"");
+        _mintBatch(owner, ids, amount, "");
     }
-     
-
-
 }
