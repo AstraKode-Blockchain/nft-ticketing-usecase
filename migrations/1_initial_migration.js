43207d3967bcd48723601db01925963081d9c0a7
Ori Bachar <oribachar98@gmail.com>
2022-11-07 14:31:42 +0200

    Update 1_initial_migration.js

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,7 @@
-const Migrations = artifacts.require("Migrations");
+const MarketPlaceMain1155 = artifacts.require("MarketPlaceMain1155");
 const Main = artifacts.require("Main");
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
   deployer.deploy(Main);
+  deployer.deploy(MarketPlaceMain1155);
 };
