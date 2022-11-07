f8d8b8f20fc56b3f1504db5c971037f920f046bb
Ori Bachar <oribachar98@gmail.com>
2022-11-07 11:40:38 +0200

    Update Main.sol

--- contracts/Main.sol
+++ contracts/Main.sol
@@ -1,9 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./CreateMarketItem.sol";
+import "./MarketItemMain.sol";
+import "./MarketPlaceMain1155.sol";
+import "./Refunded.sol";
+import "./MintFactoryMain1155.sol";
 
-abstract contract Main is CreateMarketItem {
+abstract contract Main is
+    MarketItemMain,
+    MarketPlaceMain1155,
+    Refunded,
+    MintFactoryMain1155
+{
     function createMarketItem(
         address nftContract,
         uint256[] memory tokenIds,
@@ -11,5 +19,56 @@ abstract contract Main is CreateMarketItem {
         uint256[] memory amounts
     ) public payable nonReentrant {
         _createMarketItem(nftContract, tokenIds, price, amounts);
+    }
+
+    function createMarketSale(
+        address nftContract,
+        uint256 itemId,
+        uint256[] memory _tokenIds,
+        uint256[] memory amounts
+    ) public payable nonReentrant {
+        _createMarketSale(nftContract, itemId, _tokenIds, amounts);
+    }
+
+    function fetchMarketItems()
+        public
+        view
+        returns (MarketItemData.MarketItem[] memory)
+    {
+        return _fetchMarketItems();
+    }
+
+    function addRefundParameters(
+        address nftContract,
+        uint256 maxInfection,
+        uint256 price,
+        uint256 itemId
+    ) public {
+        _addRefundParameters(nftContract, maxInfection, price, itemId);
+    }
+
+    function refundUsers(
+        address payable[] memory clients,
+        uint itemId
+    ) public payable {
+        _refundUsers(clients, itemId);
+    }
+
+    function fetchParameters(
+        uint itemId
+    ) public view returns (RefundedData.RefundParameters memory) {
+        return _fetchParameters(itemId);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    )
+        public
+        view
+        virtual
+        override(ERC1155Receiver, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
