a5303ee62d95ea9f0403f4f27d0902c63e4e4cf0
Ori Bachar <oribachar98@gmail.com>
2022-11-25 11:42:37 +0200

    Update main.js

--- test/main.js
+++ test/main.js
@@ -1,35 +1,44 @@
 const NFTContract = artifacts.require('NFTContract');
 const Main = artifacts.require('Main');
-const MarketItemMain = artifacts.require('MarketItemMain');
-const Web3 = require('web3');
-const rpcURL = 'http://172.30.64.1:7545';
-const objWeb3 = new Web3(rpcURL);
+
 const assert = require('assert');
 
 let nftContract;
 let mainContract;
-let marketItem;
+
 
 before(async () => {
   nftContract = await NFTContract.deployed();
   mainContract = await Main.deployed();
-  marketItem = await MarketItemMain.deployed();
 });
 
 contract('1. Main contract test', function (accounts) {
   it('1.1 Try to approve the NFT', async () => {
     await nftContract.setApprovalForAll(mainContract.address, true);
+    assert(await nftContract.isApprovedForAll.call(accounts[0], mainContract.address), "NFT don't approved the contract address");
+    
   });
 
   it('1.2 Check if item created', async () => {
     var tokenIds = [1];
     var price = 1;
     var amounts = [1];
-    console.log(mainContract.address)
     await mainContract.createMarketItem(
       nftContract.address,
       tokenIds,
       price,
+      amounts
+    );
+  });
+
+  it('1.3 Try to create a sale', async () => {
+    var itemId = 1;
+    var tokenIds = 1;
+    var amounts = [1];
+    await mainContract.createMarketSale(
+      nftContract.address,
+      itemId,
+      tokenIds,
       amounts
     );
   });
