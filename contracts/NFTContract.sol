9fb2151caceae1ae6f1a9f86a02c1e816bf28e32
Ori Bachar <oribachar98@gmail.com>
2022-11-25 11:42:47 +0200

    Update NFTContract.sol

--- contracts/NFTContract.sol
+++ contracts/NFTContract.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.0;
 import "../utils/ERC1155.sol";
 
 contract NFTContract is ERC1155 {
+    address private _owner;
+
     constructor(
         string memory uri,
         uint256[] memory ids,
@@ -14,6 +16,11 @@ contract NFTContract is ERC1155 {
             // "https://ipfs.moralis.io:2053/ipfs/QmS7izjgMprD3ZvP8aDBRiXbMZnxBsrUK4PJTwkcDFauij"
         )
     {
+        _owner = owner;
         _mintBatch(owner, ids, amount, "");
+    }
+
+    function getOwner() public view returns (address) {
+        return _owner;
     }
 }
